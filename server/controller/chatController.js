c3a737a864b0ee93a8e4f92ca2092e4c11637704
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-12 23:11:48 +0300

    creating the server cors and socket.io

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -8,7 +8,13 @@ const chat= asyncHandler(async (req,res) => {
  const suser= await loginModel.findOne({user_Name});
  const ruser= await loginModel.findOne({user_Name});
  if(suser && ruser){
-    
+    const chatCreated = await chatModel.create({
+        chat_id, 
+        suser_Name,
+        ruser_Name,
+        message,
+        date_time 
+    });
  }
 
 });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const db_connect = require('./DB/config');
+const cors= require('cors');
 
 const app=require('express')();
 require('dotenv').config();
 
+// middleware 
+app.use(express.urlencoded({extended: true})); // enable to receive data from frontend
+app.use(express.json());
+app.use(cors());
+
 const server=require('http').createServer(app);
 db_connect();
+
+
 const PORT_NUM = process.env.PORT || 6000;
-app.use(express.json());
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+    }
+  }); //this Socket.io helps to connect client to server
+
+  
+
+
 server.listen(PORT_NUM,() => {
     console.log(`Server listening on ${PORT_NUM}`);
 });
