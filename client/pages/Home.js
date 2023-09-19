b2b77796d1b6ae10ac63b5fbe121e1187dcc1f7a
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-19 12:47:56 +0300

    installing react-router-dom

---
+++ client/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Well Come to Home
+    </div>
+  )
+}
+
+export default Home
---
+++ client/pages/chats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const chats = () => {
+  return (
+    <div>
+      Well Cme to Chats
+    </div>
+  )
+}
+
+export default chats
