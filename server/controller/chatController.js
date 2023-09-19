7256e80841148ce81caea7612cac0e2c3c16354d
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-19 13:40:07 +0300

    importing axios

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -3,7 +3,10 @@ const asyncHandler = require('express-async-handler');
 const loginModel = require('../models/loginModel');
 const chatModel = require('../models/chatModel');
 
-const chat= asyncHandler(async (req,res) => {
+//@disc Chat 
+//@routes POST /api/chat/
+//@access public
+const chat= asyncHandler (async (req,res) => {
  const {chat_id, suser_Name,ruser_Name,message,date_time}=req.body;
  const suser= await loginModel.findOne({user_Name});
  const ruser= await loginModel.findOne({user_Name});
