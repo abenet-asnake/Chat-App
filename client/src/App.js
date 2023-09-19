c9622972d9060f6ed4cef47532096c138d4311db
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-19 12:30:05 +0300

    instaling checkrin UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Button} from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
+       <div className="App">
+       <Button colorScheme='blue'>Button</Button>
       Hello There 
     </div>
+  
+    </ChakraProvider>
   );
 }
 
