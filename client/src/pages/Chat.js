48de08961df2246c59a2f9b6559e52bc4e1c51b8
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-28 19:46:39 +0300

    Chat Model

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react';
+import axios from 'axios';
+
 
 const Chat = () => {
+// fetching data from server and client 
+const fetchChat = async () => {
+  const data = await axios.get("/api/chat");
+  console.log(data)
+};
+//useEffect is  
+useEffect(() => {
+fetchChat();
+}, []);
+
   return (
     <div>
-      Well Cme to Chats
+      Well Come  to Chats
     </div>
   )
 }
