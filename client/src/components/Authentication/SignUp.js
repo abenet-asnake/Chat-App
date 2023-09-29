9d17a7f52bda8601901dab656db84521638575b0
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-29 12:55:25 +0300

    password show and Hide feautre

--- client/src/components/Authentication/SignUp.js
+++ client/src/components/Authentication/SignUp.js
@@ -1,7 +1,9 @@
 import React , {useState} from 'react'
-import { FormControl, FormLabel, Input, VStack} from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input, 
+  InputGroup, InputRightElement, VStack} from '@chakra-ui/react';
 const SignUp = () => {
   // creating state
+const [show, setShow]=useState(false);
   const [firstName,setFirstName] = useState();
   const [lastName,setLastName] = useState();
   const [userName,setUserName] = useState();
@@ -10,6 +12,10 @@ const SignUp = () => {
   const [password,setPassword] = useState();
   const [confirmPassword,setConfirmPassword] = useState();
   const [picture,setPicture] = useState();
+
+  // handling the show and hide buttons
+  const handleClick=() => setShow(!show);
+
 
   return (
    <VStack spacing='6px'>
@@ -51,17 +57,35 @@ const SignUp = () => {
       <FormControl id='password' isRequired>
         <FormLabel>Password</FormLabel>
       </FormControl>
-      <Input
-      placeholder='Enter Your Password'
-      onChange={(error)=>setPassword(error.target.value)}/>
+         <InputGroup>
+         <Input
+             type={show ? 'text':'password' }
+             placeholder='Enter Your Password'
+             onChange={(error)=>setPassword(error.target.value)}/>
+        <InputRightElement width='4.5rem'>
+          <Button h='1.5rem' size='sm' onClick={handleClick}>
+            {show ? "Hide" : "Show"}
+          </Button>
+        </InputRightElement>
 
+         </InputGroup>
+   
 
     <FormControl id='re-password' isRequired>
         <FormLabel>Confirm password</FormLabel>
         </FormControl>
+         <InputGroup>
          <Input
-         placeholder='Confirm Password'
-         onChange={(error)=>setPassword(error.target.value)}/>
+             type={show ? 'text':'password' }
+             placeholder='Enter Your Password'
+             onChange={(error)=>setPassword(error.target.value)}/>
+        <InputRightElement width='4.5rem'>
+          <Button h='1.5rem' size='sm' onClick={handleClick}>
+            {show ? "Hide" : "Show"}
+          </Button>
+        </InputRightElement>
+
+         </InputGroup>
 
    </VStack>
   )
