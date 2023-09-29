c9911ea28e0e705b80b407564919c36b83fc8e77
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-29 11:41:54 +0300

    Making Tabs by chakra-ui/react

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,44 @@
 import React from 'react'
-
+import { Container, Box, Text,Tabs,TabList,Tab,TabPanels,TabPanel } from '@chakra-ui/react';
 const Home = () => {
   return (
-    <div>
-      Well Come to Home
-    </div>
+    <Container maxW='xl' centerContent>
+ <Box
+        display='flex'
+        justifyContent='center'
+        padding='4' 
+        bg={'whiteAlpha.100'}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+ >
+<Text fontSize="25px" color="white" fontWeight="bold">AB Chat App</Text>
+ </Box>
+ <Box 
+         bg="white" 
+         w="100%" 
+         p={4} 
+         borderRadius="lg" 
+         borderWidth="1px">
+  
+  <Tabs variant='soft-rounded' colorScheme='green'>
+  <TabList marginBottom='1em'>
+    <Tab width='50%'>Login</Tab>
+    <Tab width='50%'>Sign Up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+    {/*<Login/>*/}
+    </TabPanel>
+    <TabPanel>
+     {/*<SignUp/>*/}
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+
+ </Box>
+    </Container>
   )
 }
 
